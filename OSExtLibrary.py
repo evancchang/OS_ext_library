409d4068b278b03350bee0c59e37a21a251ec1ca
Evan Chang <chin.yeng@gmail.com>
2013-05-29 11:28:13 +0800

    added "change_directory" keyword for Robot Framework.

---
+++ OSExtLibrary.py
@@ -0,0 +1,27 @@
+#**************************************************************************************************
+#
+# [Filename]: OSExtLibrary.py
+# [Purpose ]: Additional keyword for Robot Framework.
+#
+# History:
+#**************************************************************************************************
+# VER  NAME      DATE        COMMENT
+#**************************************************************************************************
+# R00  Evan Chang 2013/05/29  Initial version.
+
+#==================================================================================================
+#
+# Import Section: add all import here
+#
+#==================================================================================================
+import os
+
+
+class OSExtLibrary:
+	def __init__(self):
+		self.original_dir = os.getcwd()
+
+	def change_directory(self, dir):
+		print "current directory : %s" % os.getcwd()
+		os.chdir(dir)
+		print "change directory to  %s" % os.getcwd()		
